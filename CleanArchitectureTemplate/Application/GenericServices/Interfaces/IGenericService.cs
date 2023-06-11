7a1dbed8fe7deb0c1104135b4369b9891edba175
Piciorus Ovidiu-Mihai <60327678+Piciorus-Ovidiu-Mihai@users.noreply.github.com>
2023-06-12 00:16:53 +0300

    add base service

---
+++ CleanArchitectureTemplate/Application/GenericServices/Interfaces/IGenericService.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.GenericServices.Interfaces
+{
+    public interface IGenericService<T>
+    {
+        void Create(T entity);
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        void Update(T entity);
+        void DeleteById(int id);
+
+        void Delete(T entity);
+    }
+}
